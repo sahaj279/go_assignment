fadf94decd62f4548cc9d2bf9cd8653f1df35a6e
Sahaj Shrivastava <sahajshrivastava.official@gmail.com>
2024-01-16 23:01:54 +0530

    updated tests

--- item/item_test.go
+++ item/item_test.go
@@ -2,15 +2,56 @@ package item
 
 import (
 	"assignment1/item/enum"
+	"errors"
 	"testing"
 )
 
+type InputItem struct {
+	name     string
+	itemType string
+	price    float64
+	quantity int
+}
+
 func TestCreateItem(t *testing.T) {
-	correctItem := Item{Name: "bread", Type: enum.Raw, Price: 40, Quantity: 100}
-	result, _ := CreateItem("bread", "raw", 40, 100)
-	if result != correctItem {
-		t.Errorf("item Creation failed")
+	tests := []struct {
+		scenario     string
+		expectedItem Item
+		input        InputItem
+		err          error
+	}{
+		{
+			scenario:     "Valid test case",
+			expectedItem: Item{Name: "bread", Type: enum.Raw, Price: 30, Quantity: 3},
+			input:        InputItem{"bread", "raw", 30, 3},
+			err:          nil,
+		},
+		{
+			scenario:     "InValid test case invalid item type",
+			expectedItem: Item{Name: "bread", Type: enum.Raw, Price: 30, Quantity: 3},
+			input:        InputItem{"bread", "ram", 30, 3},
+			err:          errors.New("ram does not belong to ItemType values"),
+		},
+		{
+			scenario:     "InValid test case invalid price and quantity",
+			expectedItem: Item{Name: "bread", Type: enum.Raw, Price: 40, Quantity: 200},
+			input:        InputItem{"bread", "raw", -30, -3},
+			err:          errors.New("item invalid: Price: negative value; Quantity: negative value."),
+		},
 	}
+
+	for _, tc := range tests {
+		result, err := CreateItem(tc.input.name, tc.input.itemType, tc.input.price, tc.input.quantity)
+		if err != nil && tc.err == nil {
+			t.Errorf("Scenario: %s \n got: %v, expected: %v", tc.scenario, err, tc.err)
+
+		}
+		if result != tc.expectedItem && tc.err == nil {
+			t.Errorf("Scenario: %s \n got: %v, expected: %v", tc.scenario, err, tc.err)
+
+		}
+	}
+
 }
 func TestCalculateSalesTax(t *testing.T) {
 	var tests = []struct {
